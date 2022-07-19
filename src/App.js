ca45773cde88f03259738ba459fb221c5a0beef2
Misael-GC <misael.gomez1623@gmail.com>
2022-07-19 13:27:21 -0500

    hook useState - cambio de modo dark & light

--- src/App.js
+++ src/App.js
@@ -2,18 +2,38 @@ import React from "react";
 import { Header } from "./components/Header";
 import { Characters } from "./components/Characters";
 import "./App.css";
+import {Headers} from './components/Headers'
 import ThemeProvider from "./context/ThemeProvider";
-import { Headers } from "./components/Headers";
+import {useState} from "react";
+
+const themeStyles = {
+  dark: {
+    background: '#282c34',
+    textColor: 'white'
+  },
+  light: {
+    background: 'white',
+    textColor: '#282c34'
+  }
+}
+
 
 function App() {
+  const [theme, setTheme] = useState('dark');
+  const toggleTheme = () => theme === 'dark'? setTheme('light') : setTheme('dark'); 
   return (
-      <ThemeProvider>
-    <div className="App">
-        <Headers/>
+    <ThemeProvider>
+    <div className="App" style={{backgroundColor: themeStyles[theme].background, color: themeStyles[theme].textColor}}>
+    <Headers/>
         <Header />
+        <button onClick={toggleTheme}>
+        Modo Pantalla
+        </button>
+        <p>Current Themes is "{theme}"</p>
         <Characters />
     </div>
-      </ThemeProvider>
+    </ThemeProvider>
+      
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeContext } from './context/ThemeContext';
 
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
