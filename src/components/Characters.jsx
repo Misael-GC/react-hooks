05ac55a1cd0d13f6dc8adfb6d5799f8bc96691cd
Misael-GC <misael.gomez1623@gmail.com>
2022-07-21 00:27:07 -0500

    Custom hooks: abstracción en la lógica de tus componentes

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,11 +1,14 @@
-import React, {useState, useEffect, useReducer, useMemo, useRef, useCallback}  from 'react';
+import React, {useState, useReducer, useMemo, useRef, useCallback}  from 'react';
 import './Characters.css';
 import { Search } from './Search.jsx';
+import { useCharacters } from '../hooks/useCharacters';
 
 
 const initialState = {
     favorites: []
 }
+
+    const API = 'https://rickandmortyapi.com/api/character/ '
 
 const favoriteReducer = (state, action) => {
     switch(action.type){
@@ -20,16 +23,11 @@ const favoriteReducer = (state, action) => {
 }
 
 const Characters = () => {
-    const [characters, setCharacters] = useState([]);
     const [favorites, dispatch] = useReducer(favoriteReducer, initialState);
     const [search, setSearch] = useState('');
     const searchInput = useRef(null);
 
-    useEffect(() =>{
-        fetch(`https://rickandmortyapi.com/api/character`)
-        .then(response => response.json())
-        .then(data => setCharacters(data.results)); 
-    }, []);
+    const characters = useCharacters(API);
 
     const handleClick = favorite => {
         dispatch({type: 'ADD_TO_FAVORITE', payload: favorite})
